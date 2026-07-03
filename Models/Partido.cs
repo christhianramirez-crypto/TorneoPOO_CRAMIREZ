088774f2dd0995887ecfeb15e598d2efebd4f677
Admin <Admin@DESKTOP-18VAGMV>
2026-07-02 22:18:36 -0500

    Agregar archivos de proyecto.

---
+++ Models/Partido.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TorneoPOO_CRAMIREZ.Models
+{
+    public class Partido
+    {
+        public Equipo Local { get; set; }
+        public Equipo Visitante { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Lugar { get; set; }
+
+        public void Programar(Equipo local, Equipo visitante, DateTime fecha, string lugar)
+        {
+            this.Lugar = lugar;
+            this.Local = local;
+            this.Visitante = visitante;
+            this.Fecha = fecha;
+            Console.WriteLine("Partido programado correctamente");
+        }
+
+        public void MostrarResumen()
+        {
+            Console.WriteLine($"Hay un partido programado entre el local {this.Local.Nombre} y el visitante {this.Visitante.Nombre} en el lugar {this.Lugar}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using TorneoPOO_CRAMIREZ.Models;
+
+Jugador objJugador1 = new Jugador();
+
+objJugador1.Nombre = "Piero Hincapié";
+objJugador1.Numero = 3;
+objJugador1.Posicion = "Defensa";
+objJugador1.Edad = 24;
+
+Jugador objJugador2 = new Jugador();
+
+objJugador2.Nombre = "Enner Valencia";
+objJugador2.Numero = 13;
+objJugador2.Posicion = "Delantero";
+objJugador2.Edad = 39;
+
+Equipo objEquipo1= new Equipo();
+
+objEquipo1.Nombre = "IDV";
+objEquipo1.Ciudad = "Quito";
+objEquipo1.Jugadores = new List<Jugador>();
+
+objEquipo1.AgregarJugador(objJugador1);
+objEquipo1.AgregarJugador(objJugador2);
+
+
+
+objEquipo1.ListarPlantilla();
+
+Jugador objJugador3 = new Jugador();
+
+objJugador3.Nombre = "Gabriel Cortes";
+objJugador3.Numero = 8;
+objJugador3.Posicion = "Medio Campo";
+objJugador3.Edad = 36;
+
+
+Jugador objJugador4 = new Jugador();
+
+objJugador4.Nombre = "Damian Diaz";
+objJugador4.Numero = 10;
+objJugador4.Posicion = "Media Punta";
+objJugador4.Edad = 40;
+
+
+Equipo objEquipo2 = new Equipo();
+
+objEquipo2.Nombre = "Barcelona";
+objEquipo2.Ciudad = "Guayaquil";
+objEquipo2.Jugadores = new List<Jugador>();
+
+objEquipo2.AgregarJugador(objJugador3);
+objEquipo2.AgregarJugador(objJugador4);
+
+
+
+objEquipo2.ListarPlantilla();
+
+Partido objPartido1 = new Partido();
+objPartido1.Programar(objEquipo1, objEquipo2, DateTime.Now, "Quito");
+objPartido1.MostrarResumen();
